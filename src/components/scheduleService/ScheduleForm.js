286ba6a167f0f1d5d4fbd686e5aaa23c62df38b2
vormc2005 <voronov.dmitry@gmail.com>
2020-11-07 10:33:20 -0500

    email feature is working

--- src/components/scheduleService/ScheduleForm.js
+++ src/components/scheduleService/ScheduleForm.js
@@ -1,115 +1,123 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-const ScheduleForm = () => {
-    const [sentEmail, setSentEmail] = useState({
-        name:'',
-        subject:"",
-        email:'',
-        phone:'',
-        year:"",
-        make:"",
-        model:"",
-        trim:'',
-        comments:"",
-        sent: false
+const ScheduleForm =()=> {
+
+  const [sentEmail, setSentEmail] = useState({
+    name: '',
+    phone:"",
+    email: "",
+    year:"",
+    make:"",
+    model:"",
+    trim:"",
+    subject:"",
+    comments:"",
+    sent: false
+  })
+
+  const {name, phone , email, year, make, model, trim, comments, subject, sent}= sentEmail
+
+  
+   
+    // handle inputs 
+
+    const handleChange = e => 
+      setSentEmail({...sentEmail, [e.target.name]:e.target.value})
+    
+ 
+    
+    // end of handle inputs   
+    
+   const  formSubmit=(e)=>{
+      e.preventDefault();    
+      
+     axios.post('http://localhost:3001/api/forma', sentEmail)
+      .then((res)=>{       
+    //  setSentEmail({...sentEmail, sent:true})
+        resetForm()   
+        
       })
-    
-      const {name, phone, email, subject, comments, year, make, model, trim, sent}= sentEmail
-    
-       
-        // handle inputs 
-    
-        const handleChange = e => 
-          setSentEmail({...sentEmail, [e.target.name]:e.target.value})
+      .catch(()=>{
+        console.log('message not send');
         
+      })
+      
      
-        
-        // end of handle inputs   
-        
-       const formSubmit=(e)=>{
-          e.preventDefault();    
-         
-          
-         axios.post('http://localhost:3001/api/forma', sentEmail)
-          .then((res)=>{
-           
-            setSentEmail({...sentEmail, sent:true})
-              resetForm()   
-          })
-          .catch(()=>{
-            console.log('message not send');
+     }
+    
+    
+    // for reseting the form data
+   const resetForm=()=>{
+     setSentEmail({
+    name: '',
+    phone:"",
+    email: "",
+    year:"",
+    make:"",
+    model:"",
+    trim:"",
+    subject:"",
+    comments:"", 
+    sent:true
+    
+      
+      })  
+     
+    }
+
+    const labelStyle = {
+      fontSize: "2vh",
+      fontWeight:"bold"
+    }
+
+    
+        return (
+            <div className="container mb-5">
+            <form onSubmit={formSubmit}>
             
-          })
-          
-         
-         }
-        
-        // for reseting the form data
-       const resetForm=()=>{
-          setSentEmail({
-            name:'',
-            phone:'',
-            message:'',
-            email:'',
-            subject:"",
-            year:"",
-            make:"",
-            model:"",
-            trim:'',
-            comments:"",
-            sent: false
-          
-          })    
-         
-        }
-        
-        const formStyle ={
-            marginBottom: "10vh"
-        }
-        
-            return (
-                <div className="container" style={formStyle}>
-                <form onSubmit={formSubmit}>
-                
-                <div className="singleItem">
-                  <label htmlFor="name"><strong>Your Name</strong></label>
-                  <input 
-                        type="text" 
-                        name="name"  
-                        value={name} 
-                        onChange={handleChange} 
-                        placeholder="your name..." />
-                </div>
-          
-                <div className="singleItem">
-                  <label htmlFor="email"><strong>Your E-mail</strong></label>
+            <div className="singleItem">
+              <label style={labelStyle} htmlFor="name">Name</label>
+              <input 
+                    type="text" 
+                    name="name" 
+                    className="name" 
+                    value={name} 
+                    onChange={handleChange} 
+                    placeholder="your name..." 
+                    required
+                    />
+            </div>
+      
+            <div className="singleItem">
+                  <label style={labelStyle} htmlFor="email"><strong>Your E-mail</strong></label>
                   <input 
                       type="email" 
                       name="email" 
                       className="email" 
                       value={email} 
                       onChange={handleChange} 
-                      placeholder="Enster your e-mail" />
+                      placeholder="Enster your e-mail" 
+                      required
+                      />
                 </div>
-          
+      
                 <div className="singleItem">
-                  <label htmlFor="phone"><strong>Phone</strong></label>
+                  <label style={labelStyle} htmlFor="phone"><strong>Phone</strong></label>
                   <input 
                       type="tel" 
                       name="phone" 
                       className="phone" 
                       value={phone} 
                       onChange={handleChange} 
-                      placeholder="your phone..." required />
+                      placeholder="your phone..."  />
                 </div>
-                <hr></hr>
                 <h2>Vehicle Inforamtion</h2>
                 
                 <div className="row ml-1">
                    
                 <div className="singleItem">
-                  <label htmlFor="year"><strong>Year</strong></label>
+                  <label style={labelStyle} htmlFor="year"><strong>Year</strong></label>
                   <input 
                         type="number" 
                         name="year" 
@@ -119,7 +127,7 @@ const ScheduleForm = () => {
                         placeholder="Vehicle year" />
                 </div>
                 <div className="singleItem ml-2">
-                  <label htmlFor="make"><strong>Make</strong></label>
+                  <label style={labelStyle} htmlFor="make"><strong>Make</strong></label>
                   <input 
                         type="text" 
                         name="make" 
@@ -128,9 +136,8 @@ const ScheduleForm = () => {
                         onChange={handleChange} 
                         placeholder="Vehicle make" />
                 </div>
-
                 <div className="singleItem ml-2">
-                  <label htmlFor="model"><strong>Model</strong></label>
+                  <label style={labelStyle} htmlFor="model"><strong>Model</strong></label>
                   <input 
                         type="text" 
                         name="model" 
@@ -139,9 +146,10 @@ const ScheduleForm = () => {
                         onChange={handleChange} 
                         placeholder="Enter desired vehicle model" />
                 </div>
-
-                <div className="singleItemml-2">
-                  <label htmlFor="trim"><strong>Trim</strong></label>
+      
+            
+            <div className="singleItemml-2">
+                  <label style={labelStyle} htmlFor="trim"><strong>Trim</strong></label>
                   <input 
                         type="text" 
                         name="trim" 
@@ -151,10 +159,8 @@ const ScheduleForm = () => {
                         placeholder="Enter trim if any" />
                 </div>
                 </div>
-
-
                 <div className="singleItem">
-                  <label htmlFor="subject"><strong>Subject</strong></label>
+                  <label style={labelStyle} htmlFor="subject"><strong>Subject</strong></label>
                   <input 
                       type="text" 
                       name="subject" 
@@ -165,7 +171,7 @@ const ScheduleForm = () => {
                 </div>
           
                 <div className="textArea singleItem">
-                <label htmlFor="message"><strong>Your message</strong></label>
+                <label style={labelStyle} htmlFor="message"><strong>Your message</strong></label>
                   <textarea 
                     style={{height:"20vh"}}
                       name="comments" 
@@ -176,17 +182,21 @@ const ScheduleForm = () => {
                       onChange={handleChange}></textarea>
                 </div>
           
-                 
-                 <div className={sent?'msg msgAppear':'msg'}>Message has been sent</div>
-                <div className="btn">
-                <button type="submit">Submit</button>
-                </div>
-          
-          
-               </form>
-              </div>
-            )
-        
+      
+             
+            {sent &&
+
+            <div >Message has been sent</div>
+            } 
+            <div >
+            <button className="btn btn-primary" type="submit">Send Request</button>
+            </div>     
+      
+           </form>
+          </div>
+        )
+    
 }
 
-export default ScheduleForm
+
+export default ScheduleForm;
